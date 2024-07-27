e0c5be6aa40cba1c6bdcec5c98be78acc013c0b5
Shivansh <Shivanshpandeycse942@gmail.com>
2024-07-27 22:08:29 +0530

    used lombok

--- application/application/src/main/java/com/example/application/controller/entry/journalentry.java
+++ application/application/src/main/java/com/example/application/controller/entry/journalentry.java
@@ -1,46 +1,21 @@
 package com.example.application.controller.entry;
 
+import lombok.Data;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
+
+
 @Document("journal")
+@Data
 public class journalentry {
     @Id
     private ObjectId id;
     private String tittle;
     private String content;
-
-    public LocalDateTime getDate() {
-        return date;
-    }
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-
     private LocalDateTime date;
-    public ObjectId getId() {
-        return id;
-    }
 
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
 
-    public String getTittle() {
-        return tittle;
-    }
-
-    public void setTiitle(String tittle) {
-        this.tittle = tittle;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
 }
--- application/application/src/main/java/com/example/application/controller/jounalentrycontroller.java
+++ application/application/src/main/java/com/example/application/controller/jounalentrycontroller.java
@@ -5,7 +5,7 @@ import com.example.application.controller.entry.services.journalservice;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,33 +24,42 @@ private journalservice journalservice;
     public List<journalentry> getAll(){
        return journalservice.getAll();
     }
+    
     @GetMapping("id/{myid}")
-    public ResponseEntity<journalentry> getJournalEntrybyid(@PathVariable ObjectId myid){
-        Optional<journalentry> journalentry=journalservice.findById(myid);
-       if(journalentry.isPresent()){
-           return new ResponseEntity<>(journalentry.get(), HttpStatus.OK);
-       }
-       return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    public ResponseEntity<journalentry> getJournalEntrybyid(@PathVariable ObjectId myid)
+    {
+        Optional<journalentry> journalentryOptional = journalservice.findById(myid);
+        return journalentryOptional.map(journalentry -> new ResponseEntity<>(journalentry,HttpStatus.OK)).orElseGet(()->new ResponseEntity<>(HttpStatus.NOT_FOUND));
+
     }
     @PostMapping
-    public boolean createEntry(@RequestBody journalentry myentry){
-        myentry.setDate(LocalDateTime.now());
-        journalservice.saveEntry(myentry);
-        return true;
+    public ResponseEntity<journalentry> createEntry(@RequestBody journalentry myentry){
+       try {
+           myentry.setDate(LocalDateTime.now());
+           journalservice.saveEntry(myentry);
+           return new ResponseEntity<>(myentry, HttpStatus.CREATED);
+       }
+       catch (Exception e){
+           return new ResponseEntity<>(myentry, HttpStatus.BAD_REQUEST);
+
+
+       }
     }
+
 @DeleteMapping("id/{myId}")
-    public boolean deleteEntrybyid(@PathVariable ObjectId myId){
+    public ResponseEntity<?> deleteEntrybyid(@PathVariable ObjectId myId){
         journalservice.deletebyid(myId);
-        return true;
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 }
 @PutMapping("id/{id}")
-    public journalentry updatejournalentrybyid(@PathVariable ObjectId id,@RequestBody journalentry newentry){
+    public ResponseEntity<?> updatejournalentrybyid(@PathVariable ObjectId id,@RequestBody journalentry newentry){
     journalentry old=journalservice.findById(id).orElse(null);
     if(old !=null){
-        old.setTiitle(newentry.getTittle()!=null && !newentry.getTittle().equals("")? newentry.getTittle() : old.getTittle());
+        old.setTittle(newentry.getTittle()!=null && !newentry.getTittle().equals("")? newentry.getTittle() : old.getTittle());
         old.setContent(newentry.getContent() !=null && !newentry.equals("")?newentry.getContent(): old.getContent());
+        journalservice.saveEntry(old);
+        return new ResponseEntity<>(old,HttpStatus.OK);
     }
-    journalservice.saveEntry(old);
-            return old;
+    return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 }
 }
